8ca93dd43aa605c65a2f72db72d3d39f46a39abe
FahimFBA <fahimbinamin@gmail.com>
2023-03-25 15:30:57 +0600

    Boilerplate class deleted

--- src/main/java/com/game/guessingnumbergame/VerdictScreen.java
+++ src/main/java/com/game/guessingnumbergame/VerdictScreen.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class VerdictScreen extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("verdictscreen-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainGameScreen.class.getResource("verdictscreen-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Guess The Number!!");
         stage.setScene(scene);
